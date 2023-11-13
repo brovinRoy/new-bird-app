4d5112e8ce6a393152c87b04b5705a5102c08089
brovinRoy <brovinroy@gmail.com>
2023-11-13 13:58:38 +0530

    changed hell to hello!

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello!</h1>
+      <Birds />
     </div>
-  );
+  )
 }
-
-export default App;
+function Birds() {
+  const Bird1 = {
+    Name: 'Parrot',
+    Color: 'Green',
+    icon:'🦜'
+  }
+  const Bird2 = {
+    Name: 'Duck',
+    Color: 'White',
+    icon:'🦆'
+  }
+  return (
+    <div>
+      <h2>I am a {Bird1.Name} and my color is {Bird1.Color}{Bird1.icon}</h2>
+      <h2>I am a {Bird2.Name} and my color is {Bird2.Color}{Bird2.icon}</h2>
+    </div>
+  )
+}
+export default App
